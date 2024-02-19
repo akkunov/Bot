0b6dc4acd8b4a18866ab07920eac18419f91328d
akkunov <akunov313131@gmail.com>
2024-02-19 23:00:57 +0600

    add animated button

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import css from './item.module.css'
 
 export default function Item({name, image, price}) {
 
+    const [isClicked, setIsClicked] = useState(false);
+    const [cols, setCols] = useState(null)
+    const handleAddButtonClick = () => {
+        setIsClicked(true);
+    };
+
+    const handleClick = () => {
+        setCols(prev => prev + 1 )
+    }
+
   return (
     <div className={css.box}>
+    
         <div className={css.imageBox}>
             <img src={image} />
         </div>
@@ -13,6 +24,19 @@ export default function Item({name, image, price}) {
                 <span>{name}</span>
                 <span>{price}</span>
             </div>
+            <div className={css.btnGroup}>
+            {cols? (
+                <>
+                    <button className={css.animatedButtonMinus}>-</button>
+                    <button className={css.animatedButton}>+</button>
+                </>
+                   
+
+            ): ( <button className={css.button} onClick={handleClick}>
+                    ADD
+                </button>) } 
+            </div>
+           
     </div>
   )
 }
